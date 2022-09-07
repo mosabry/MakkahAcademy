a61cd5cf45fa18c7a210564cb8a1d6609b9674fe
Mohamed Sabry <msabry809@gmail.com>
2022-09-07 20:36:45 +0200

    change from noor mobin to makkah academy

--- layouts/default.vue
+++ layouts/default.vue
@@ -63,7 +63,7 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'نــور مبين',
+      title: 'اكاديمية مكة',
     }
   },
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - noor-elmobin',
-    title: 'Noor Elmobin',
+    titleTemplate: '%s - Learn Quran',
+    title: 'Makkah Academy',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
