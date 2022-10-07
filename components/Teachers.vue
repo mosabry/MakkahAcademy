dd41c0e6432ed1c676084d7a12132aed82162e67
Mohamed Sabry <msabry809@gmail.com>
2022-10-08 00:59:16 +0200

    add avatar and info to teachers

--- components/Teachers.vue
+++ components/Teachers.vue
@@ -3,18 +3,19 @@
         <h2 class='mb-5 py-3'>هيئة التدريس</h2>
         <v-row justify="center">
             <v-col v-for="(teacher, i) in teachers" :key="i" cols="12" sm="8" md="5" lg="4" xl="3" align-self="center">
-                <v-card :color="teacher.color" class="mx-auto my-6" mx-width="374" dark>
+                <v-card :color="teacher.color" class="mx-auto my-6 px-1" mx-width="374" dark>
                     <template slot="progress">
                         <v-progress-linear color="deep-purple" height="10" indeterminate></v-progress-linear>
                     </template>
 
-                    <v-avatar class="ma-3" size="125" tile>
-                        <v-img class="rounded-circle" :src="teacher.image"></v-img>
-                    </v-avatar>
+                    <div class="text-center">
+                        <v-avatar class="ma-3" size="135" tile>
+                            <v-img class="mx-auto rounded-circle" :src="teacher.image"></v-img>
+                        </v-avatar>
+                    </div>
+                    <v-card-title class="text-h5 text-md-h6 text-sm-subtitle-1 text-xs-subtitle-1">المعلم/ {{teacher.name}}</v-card-title>
 
-                    <v-card-title class="text-h5">الشيخ/ {{teacher.name}}</v-card-title>
-
-                    <v-crad-text>{{teacher.about}}</v-crad-text>
+                    <v-card-text class="text-start">{{teacher.about}}</v-card-text>
 
                     <v-divider class="mx-4"></v-divider>
                     <v-btn v-if="teacher.yotube === ''" icon>
@@ -22,11 +23,10 @@
                     </v-btn>
 
                     <v-btn v-else icon :href="teacher.yotube" target="_blank">
-                        <v-icon large style="color: #FF0000">
+                        <v-icon style="color: #fff">
                             mdi-youtube
                         </v-icon>
                     </v-btn>
-
                 </v-card>
             </v-col>
         </v-row>
@@ -40,37 +40,58 @@ export default {
             teachers: [
                 {
                     name: "عماد أنور",
-                    image: "https://t3leem.vercel.app/assets/img/owners/person-1.png",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula dui bibendum nunc cursus, in.",
-                    color: '#1F7087',
+                    image: "https://i.imgur.com/ymITM4n.png",
+                    about: "حاصل على درجتي الماجستير والدكتوراة في التفسير وعلوم القرآن الكريم. مدرب معتمد بقسم القراءات والتجويد بالجمعية الخيرية لتحفيظ القرآن الكريم بالخبر بالمملكة العربية السعودية",
+                    color: '#1DA39A',
                     yotube: "https://www.youtube.com/channel/UCZ-TqjQwQfURaDWv0VcF3CA"
                 },
                 {
                     name: "محمد فاروق",
-                    image: "https://t3leem.vercel.app/assets/img/owners/person-2.png",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula dui bibendum nunc cursus, in.",
-                    color: "#1F7087",
+                    image: "https://i.imgur.com/kIktEVh.png",
+                    about: "حاصل على درجة الماجستير في التربية. مدرب معتمد بجمعية تحفيظ القرآن الكريم بالخبر بالمملكة العربية السعودية",
+                    color: "#1DA39A",
                     yotube: "https://www.youtube.com/channel/UC9cGScMGbz89eq3u2v-rjGA"
                 },
                 {
                     name: "أحمد فودة",
                     image: "https://t3leem.vercel.app/assets/img/owners/person-3.png",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula dui bibendum nunc cursus, in.",
-                    color: "#00DC82",
+                    about: "حاصل على درجة الماجستير في التربية من الأزهر الشريف. المشرف التعليمي بمجمع سعد بن صليب العتيبي القرآني بجامع خادم الحرمين الشريفين بالخبر",
+                    color: "#1DA39A",
                     yotube: "https://www.youtube.com/channel/UCC5pdvevmhjV9syAQt_NsuQ"
                 },
                 {
                     name: "حافظ رضوان",
                     image: "https://i.imgur.com/HJXCAmn.png",
-                    about: "خريج معهد القراءات ومعلم القراءات بالأزهر الشريف والكبرى ومجاز بالقراءات العشرى الصغرى",
-                    color: "#00DC82",
+                    about: "خريج معهد القراءات ومعلم القراءات بالأزهر الشريف والكبرى ومجاز بالقراءات العشر الصغرى. ومدرب معتمد بقسم القراءات والتجويد بالجمعية الخيرية لتحفيظ القرآن الكريم بالخبر بالمملكة العربية السعودية",
+                    color: "#2E7D32",
+                    yotube: ""
+                },
+                {
+                    name: "أحمد بركات",
+                    image: "https://t3leem.vercel.app/assets/img/owners/person-3.png",
+                    about: "مجاز بقراءة عاصم بروايه حفص وشعبة ورواية ورش وأصحاب الصلة وحمزة والسوسي. مدرب معتمد بجمعية تحفيظ القرآن الكريم بالخبر بالمملكة العربية السعودية",
+                    color: "#2E7D32",
+                    yotube: ""
+                },
+                {
+                    name: "أحمد المزين",
+                    image: "https://i.imgur.com/WG7EO0j.png",
+                    about: "حاصل على معهد القراءات بالأزهر الشريف. حاصل على درجة الليسانس في القراءات وعلوم القران الكريم. مجاز بالقراءات العشر الصغرى والكبرى والعديد من المتون العلمية",
+                    color: "#2E7D32",
+                    yotube: ""
+                },
+                {
+                    name: "حنان أبو الدهب",
+                    image: "https://t3leem.vercel.app/assets/img/owners/person-3.png",
+                    about: "مجازة بالقراءات العشر. حاصلة على درجة الماجستير في علوم القرآن الكريم من الجامعة الإسلامية ببروكسل ",
+                    color: "#2E7D32",
                     yotube: ""
                 },
                 {
                     name: "عمرو فايق",
                     image: "https://t3leem.vercel.app/assets/img/owners/person-4.png",
-                    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula dui bibendum nunc cursus, in.",
-                    color: "#00DC82",
+                    about: "حاصل على عدة إجازات في القراءات. اجادة التعليم باللغة الانجليزية",
+                    color: "#2E7D32",
                     yotube: "https://www.youtube.com/channel/UCkRGInlEPYfI2c0K7aTorkg"
                 },
             ]
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,14 @@
 <template>
   <div>
-    <HeroVue :title="title" :verses="verses" />
-
-    <!-- <section class="green lighten-5 py-4">
-      <h1>بَلِّغُوا عَنِّي ولو آيَةً</h1>
-    </section> -->
+    <HeroVue />
 
     <main>
-      <v-container class="text-center">
-        <div class="goal-div my-6 py-3">
+      <v-container>
+        <div class="goal-div text-center my-6 py-3">
           <h1>أكاديمية مكة المكرمة لعلوم القرآن الكريم</h1>
         </div>
 
-        <!-- <section class="my-6">
-
-          <h2 class='mb-5 py-3 text-decoration-underline'>القراءات</h2>
-          <v-row justify="center">
-            <v-col v-for="(Qeraat, i) in Qeraats" :key="i" xs="12" sm="8" md="5">
-              <v-card :color="Qeraat.color" dark>
-                <div class="d-flex flex-no-wrap justify-space-between">
-                  <div>
-                    <v-card-title class="text-h5" v-text="Qeraat.name"></v-card-title>
-
-                    <v-card-actions>
-                      <v-btn class="ml-2 mt-3" fab icon height="40px" right width="40px">
-                        <v-icon>mdi-play</v-icon>
-                      </v-btn>
-                    </v-card-actions>
-                  </div>
-
-                  <v-avatar class="ma-3" size="125" tile>
-                    <v-img :src="Qeraat.src"></v-img>
-                  </v-avatar>
-                </div>
-              </v-card>
-            </v-col>
-          </v-row>
-        </section> -->
-
         <TeachersVue />
-
       </v-container>
     </main>
   </div>
@@ -47,39 +16,10 @@
 
 <script>
 import HeroVue from '@/components/Hero.vue'
-import TeachersVue from '../components/Teachers.vue';
+import TeachersVue from '@/components/Teachers.vue';
 
 export default {
   name: 'IndexPage',
-  components: { HeroVue, TeachersVue },
-  data() {
-    return {
-      title:
-        '﴿ إِنَّ هَٰذَا الْقُرْآنَ يَهْدِي لِلَّتِي هِيَ أَقْوَمُ وَيُبَشِّرُ الْمُؤْمِنِينَ الَّذِينَ يَعْمَلُونَ الصَّالِحَاتِ أَنَّ لَهُمْ أَجْرًا كَبِيرًا﴾',
-      verses: '[ الإسراء: 9]',
-      Qeraats: [
-        {
-          color: '#1F7087',
-          src: 'https://qurancomplex.gov.sa/wp-content/uploads/2019/09/warsh.jpg',
-          name: 'ورش عن نافع',
-        },
-        {
-          color: '#7BC6FF',
-          src: 'https://qurancomplex.gov.sa/wp-content/uploads/2019/09/qalon01.jpg',
-          name: 'قالون عن نافع'
-        },
-        {
-          color: '#2E7D32',
-          src: 'https://qurancomplex.gov.sa/wp-content/uploads/2019/09/hafs.jpg',
-          name: 'حفص عن عاصم',
-        },
-        {
-          color: '#00DC82',
-          src: 'https://qurancomplex.gov.sa/wp-content/uploads/2019/09/shuba01.jpg',
-          name: 'شعبة عن عاصم',
-        },
-      ],
-    }
-  },
+  components: { HeroVue, TeachersVue }
 }
 </script>
